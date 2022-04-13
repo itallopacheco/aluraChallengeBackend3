80832443eda24c7904b099c37e6cd4fd038c7962
Itallo Pacheco <istmujin6@gmail.com>
2022-04-13 00:19:03 -0300

    [📋] Leitura de arquivos CSV

--- analisar_transacoes/models.py
+++ analisar_transacoes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Transacao(models.Model):
+    bancoOrigem = models.CharField(max_length=150, verbose_name='Banco de Origem')
+    agenciaOrigem = models.CharField(max_length=3, verbose_name='Agencia de Origem')
+    contaOrigem = models.CharField(max_length=7, verbose_name='Conta de Origem')
+    bancoDestino = models.CharField(max_length=150, verbose_name='Banco de Destino')
+    agenciaDestino = models.CharField(max_length=3, verbose_name='Agencia de Destino')
+    contaDestino = models.CharField(max_length=7, verbose_name='Conta de Destino')
+    valorTransacao = models.DecimalField(max_length=None, max_digits=2, decimal_places=2)
+    dataHora = models.DateTimeField(auto_now=False, auto_now_add=False)
--- analisar_transacoes/views.py
+++ analisar_transacoes/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from analisar_transacoes.models import Transacao
 from analisar_transacoes.forms import UploadFileForm
+import csv
+import codecs
+
 
 def upload_file(request,*args,**kwargs):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
         file_size = uploaded_file.size / (1024*1024)
-        print(uploaded_file.name)
-        print(round(file_size,2), "MB")
+
+        data = codecs.iterdecode(uploaded_file, 'utf8')
+
+        csv_reader = csv.reader(data, delimiter=',')
+        for row in csv_reader:
+            print(row)
+
+
+
     return render(request, "analisar_transacoes/form_transacoes.html", locals())
 
